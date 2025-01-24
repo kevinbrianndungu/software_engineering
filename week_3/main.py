1a1411853d8e701e76ef9bf8ac95a9c545279dfa
kevinbrianndungu <kevinbrianndungu@gmail.com>
2025-01-24 22:35:34 +0300

    weekly

---
+++ week_3/main.py
@@ -0,0 +1,15 @@
+def calculate_discount(price,discount_percent)
+    final_price = price - discount
+    percentage_dicount = discount/100 *price
+    
+    if percentage_discount =>20% :
+        print(price)
+    else:
+        print(final_price)
+
+    return final_price,price
+    
+price = float(input("whats the buying price:"))
+discount = float(input("state the discount give"))
+
+final_price = calculate
