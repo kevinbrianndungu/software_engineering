6879d64e603cd48b7ac55f4bec44806c2cfcc1d0
kevinbrianndungu <kevinbrianndungu@gmail.com>
2025-01-31 02:06:05 +0300

    jokes

---
+++ week_1/joke.py
@@ -0,0 +1,19 @@
+import random
+
+# List of programming and Python jokes
+jokes = [
+    "Why do Python programmers prefer dark mode? Because light attracts bugs!",
+    "Why did the developer go broke? Because he used up all his cache!",
+    "Why do Java programmers wear glasses? Because they don’t see sharp!",
+    "What do you call a snake that works for the government? A Python official.",
+    "Why did the function break up with the loop? It felt like it was stuck in an infinite relationship!",
+    "How do you comfort a JavaScript bug? You console it.",
+    "Why did the Python programmer need glasses? Because he couldn't C!",
+    "What’s a programmer’s favorite place to hang out? The Stack Overflow.",
+    "Why did the coder quit his job? He didn’t get arrays!",
+    "Why do Python devs hate nature? Too many trees (recursion)!",
+]
+
+# Randomly select and print a joke
+print(random.choice(jokes))
+
